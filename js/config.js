d292a61e75e2756b21963a700430f9c1a406fd11
Anna Rieder <Riederanna@aol.com>
2016-11-06 20:01:40 +0100

    test

---
+++ js/config.js
@@ -0,0 +1,60 @@
+
+const config = {"general":{"name":"General","phrases":[{"name":"...","text":"Hallo!"},{"name":"...","sticky":true,"text":"ja"},{"name":"...","sticky":true,"text":"nein"}]},"questions":{"name":"questions","phrases":[{"name":"...","text":"Wie heißt du?"},{"name":"...","sticky":true,"text":"Magst du auch gerne? "}]},"error":{"name":"error","phrases":[{"name":"...","text":"Uppsi"},{"name":"...","sticky":true,"text":"Ich glaube ich habe einen Schluckauf"}]},"recent":{"name":"recent","phrases":[{"name":"...","text":"1"},{"name":"...","text":"2"},{"name":"...","text":"3"},{"name":"...","text":"4"},{"name":"...","text":"5"}]}};
+
+
+/*
+
+const config = {
+    general: {
+        name: 'General',
+        phrases: [
+            {
+                "name": "...",
+                "text": "Hallo!"
+            },{
+                "name": "...",
+                "sticky": true,
+                "text": "ja"
+            },
+            {
+                "name": "...",
+                "sticky": true,
+                "text": "nein"
+            }
+        ]
+    },
+    questions: {
+        name: 'questions',
+        phrases: [
+            {
+                "name": "...",
+                "text": "Wie heißt du?"
+            },{
+                "name": "...",
+                "sticky": true,
+                "text": "Magst du auch gerne? "
+            }
+        ]
+    },
+    error: {
+        name: 'error',
+        phrases: [
+            {
+                "name": "..",
+                "text": "Uppsi"
+            },{
+                "name": "...",
+                "sticky": true,
+                "text": "Ich glaube ich habe einen Schluckauf"
+            }
+        ]
+    },
+    recent: {
+        name: 'recent',
+        phrases: [
+
+        ]
+    }
+
+};
+*/
